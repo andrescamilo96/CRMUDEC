aaefcff115ce6367c577d87c5d299a8acd76b808
Unknown <kmiloandres9605@gmail.com>
2018-04-11 18:56:25 -0500

    Index pagina
    index

--- public/js/app.js
+++ public/js/app.js
@@ -990,7 +990,31 @@ window.Vue = __webpack_require__(35);
 Vue.component('example-component', __webpack_require__(38));
 
 var app = new Vue({
-  el: '#app'
+	el: '#app'
+});
+
+$(document).ready(function () {
+	$('#myCarousel').carousel({
+		interval: 4000
+	});
+
+	var clickEvent = false;
+	$('#myCarousel').on('click', '.nav a', function () {
+		clickEvent = true;
+		$('.nav li').removeClass('active');
+		$(this).parent().addClass('active');
+	}).on('slid.bs.carousel', function (e) {
+		if (!clickEvent) {
+			var count = $('.nav').children().length - 1;
+			var current = $('.nav li.active');
+			current.removeClass('active').next().addClass('active');
+			var id = parseInt(current.data('slide-to'));
+			if (count == id) {
+				$('.nav li').first().addClass('active');
+			}
+		}
+		clickEvent = false;
+	});
 });
 
 /***/ }),
